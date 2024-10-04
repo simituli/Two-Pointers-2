ac68afcdb9a87d3ea041d0667668bdd510c652bf
simituli <anupama.tuli@gmail.com>
2024-10-03 21:21:14 -0700

    Update Sample.java
    update problem 1 solution

--- Sample.java
+++ Sample.java
@@ -5,3 +5,27 @@
 // Three line explanation of solution in plain english
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int slow = 0, fast=0, k=2; 
+        int count = 0;
+        while (fast<nums.length)
+        {
+            if(fast>0&&nums[fast]==nums[fast-1])
+            {
+                count++;
+            }
+            else
+            {
+                count=1;
+            }
+            if(count<=k)
+            {
+                nums[slow]=nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
