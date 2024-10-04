8b908308ae11b32329cc53a1fccbc0d8eeb6ea55
simituli <anupama.tuli@gmail.com>
2024-10-03 21:22:38 -0700

    Update Sample.java
    update the time and space complexity

--- Sample.java
+++ Sample.java
@@ -1,9 +1,9 @@
 
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
 // Three line explanation of solution in plain english
-
+//slow pointer and fast pointer solution
 // Your code here along with comments explaining your approach
 class Solution {
     public int removeDuplicates(int[] nums) {
